7ba2ce76bdf071d74afa3317e3d6393536f53244
mason <qxc@mail.dlut.edu.cn>
2019-01-11 17:35:24 +0800

    递归回溯通解

--- basic/src/main/java/com/qinxc/basic/StringSub.java
+++ basic/src/main/java/com/qinxc/basic/StringSub.java
@@ -10,18 +10,15 @@ import java.util.*;
 
 public class StringSub {
 
-    public static ArrayList<String> target = new ArrayList(Arrays.asList("a", "b", "c"));
-    //要组合，不管顺序
-//    public static Set<Set<String>> resultSet = new HashSet<>();
-    //要排列，需要顺序
+    public static ArrayList<String> target = new ArrayList(Arrays.asList("a", "b", "c", "c"));
     public static Set<List<String>> resultSet = new HashSet<>();
 
     public static void main(String[] args) {
-        Set<String> remainSet = new HashSet<>(target);
+        List<String> remainSet = new LinkedList<>(target);
         for (int i = 0; i <= target.size(); i++) {
-//            List<String> current = new ArrayList<>();
-//            getSubStr(i, current, remainSet);
-            getSubStr2(i);
+            List<String> current = new ArrayList<>();
+            getSubStr(i, current, remainSet);
+//            getSubStr2(i);
         }
 
         //只要长度为2的
@@ -33,13 +30,12 @@ public class StringSub {
         System.out.println(resultSet);
     }
 
-    public static void getSubStr(int length, List<String> current, Set<String> remainSet) {
+    public static void getSubStr(int length, List<String> current, List<String> remainSet) {
 
         if (current.size() == length) {
-            //1、要组合，不管顺序
-//            Set<String> temp = new HashSet<>(current);
-            //2、要排列，要顺序
             List<String> temp = new ArrayList<>(current);
+            //组合就排序去重 排列不排序
+            Collections.sort(temp);
             resultSet.add(temp);
             return;
         }
@@ -62,15 +58,16 @@ public class StringSub {
         Stack<List<String>> stack = new Stack<>();
         stack.push(new ArrayList<>());
         while (!stack.empty()) {
-            //排列用list 组合用set去重
             List<String> temp = stack.pop();
             if (temp.size() <= 3) {
                 if (temp.size() == length) {
+                    //组合就排序去重 排列不排序
+                    Collections.sort(temp);
                     resultSet.add(temp);
                 }
                 //去除temp中已有的元素
                 List<String> remain = new ArrayList<>(target);
-                for(int i=0;i<temp.size();i++) {
+                for (int i = 0; i < temp.size(); i++) {
                     remain.remove(temp.get(i));
                 }
                 for (int i = 0; i < remain.size(); i++) {
