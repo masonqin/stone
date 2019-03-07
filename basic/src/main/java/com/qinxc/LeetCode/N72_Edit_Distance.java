51a819f3ab85e86af3f41c02dfcdee9612ff2e9a
mason <qxc@mail.dlut.edu.cn>
2019-03-07 10:12:29 +0800

    save

---
+++ basic/src/main/java/com/qinxc/LeetCode/N72_Edit_Distance.java
@@ -0,0 +1,46 @@
+package com.qinxc.LeetCode;
+
+/**
+ * @author qxc
+ * @date 2019/3/5.
+ */
+
+/**
+ * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+ * <p>
+ * You have the following 3 operations permitted on a word:
+ * <p>
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * Example 1:
+ * <p>
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation:
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * Example 2:
+ * <p>
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * Explanation:
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ */
+
+
+public class N72_Edit_Distance {
+
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
